4f08fcb45ede7587a827c57facc83f7fb0d0bad5
manni.ojha <manni.ojha@rakuten.com>
2021-05-17 01:25:51 +0530

    Finalized Expense Tracker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
+import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './../NewExpense/ExpensesFilter';
-import './Expenses.css';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import { useState } from 'react';
 
 const Expenses = (props) => {
@@ -13,12 +14,11 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    
-
     return (   
             <div>
                 <Card className="expenses">
                     <ExpensesFilter selectedYear={filteredYear} onChangeFilter = {filterChangeHandler} />
+                    <ExpensesChart expenses={filteredExpenses} />
                     <ExpensesList items={filteredExpenses} />
                 </Card>
             </div>
